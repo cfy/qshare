24ca243fe37b9bbcdc520eee84543648d906a0ae
pharus <pintomachado@tecnico.ulisboa.pt>
2016-07-31 18:55:29 +0100

    Small change

--- app.js
+++ app.js
@@ -6,16 +6,18 @@
 
     function qshare(port, folder) {
         var express = require('express'),
+            morgan = require('morgan'),
             path = require('path'),
             serveIndex = require('serve-index'),
             serveStatic = require('serve-static'),
             dir,
             app = express();
 
-
         port = port || 1337;
         dir = folder.charAt(0) === '/' ? folder :
                                        path.resolve(process.cwd(), folder);
+
+        app.use(morgan('common'));
         app.use('/', serveIndex(dir, {'icons': true}));
         app.use('/', serveStatic(dir));
         app.listen(port, function () {
